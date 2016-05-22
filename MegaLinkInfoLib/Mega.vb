bc4db4ae1fc75c63a5546855c0009a2ab26f2df9
sisimi.sidc@gmail.com <sisimi.sidc@gmail.com>
2016-05-22 21:36:42 +0800

    MegaLinkInfoLib Project

---
+++ MegaLinkInfoLib/Mega.vb
@@ -0,0 +1,9 @@
+﻿
+Public Class Mega
+    Public Shared Function GetUrlInfo(ByVal url As String) As Conexion.InformacionFichero
+        Dim FileID As String = URLExtractor.ExtraerFileID(url)
+        Dim FileKey As String = URLExtractor.ExtraerFileKey(url)
+        Dim Config = New Configuracion()
+        Return Conexion.ObtenerInformacionFichero(Config, FileID, FileKey, False)
+    End Function
+End Class

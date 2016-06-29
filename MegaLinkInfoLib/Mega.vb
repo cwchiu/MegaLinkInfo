4389a7d02c5add651f99b9cc74f499be88623610
Chui-Wen Chiu <sisimi.pchome@gmail.com>
2016-06-29 15:46:58 +0800

    1. add GetUrlInfo2 and GetUrlInfo3 2. support check mega folder

--- MegaLinkInfoLib/Mega.vb
+++ MegaLinkInfoLib/Mega.vb
@@ -6,4 +6,43 @@ Public Class Mega
         Dim Config = New Configuracion()
         Return Conexion.ObtenerInformacionFichero(Config, FileID, FileKey, False)
     End Function
+
+    Public Shared Function GetUrlInfo2(ByVal url As String) As Generic.List(Of Conexion.InformacionFichero)
+        Dim links As New Generic.HashSet(Of Conexion.InformacionFichero)
+        If URLExtractor.IsMegaFolder(url) Then
+            Dim FolderID As String = URLExtractor.ExtraerFileID(url)
+            Dim FolderKey As String = URLExtractor.ExtraerFileKey(url)
+            For Each FileURL In MegaFolderHelper.RetrieveLinksFromFolder(FolderID, FolderKey)
+                links.Add(GetUrlInfo(FileURL.URL))
+            Next
+        Else
+            links.Add(GetUrlInfo(url))
+        End If
+        Return links.ToList
+    End Function
+
+    Public Shared Function GetUrlInfo3(ByVal url As String) As Boolean
+        Dim info As Conexion.InformacionFichero
+        If URLExtractor.IsMegaFolder(url) Then
+            Dim FolderID As String = URLExtractor.ExtraerFileID(url)
+            Dim FolderKey As String = URLExtractor.ExtraerFileKey(url)
+            For Each FileURL In MegaFolderHelper.RetrieveLinksFromFolder(FolderID, FolderKey)
+                info = GetUrlInfo(FileURL.URL)
+                Exit For
+            Next
+
+        Else
+            info = GetUrlInfo(url)
+        End If
+
+        If IsNothing(info) Then
+            Return False
+        End If
+
+        If info.Errtxt = "" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
 End Class
